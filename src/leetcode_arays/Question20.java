20c752903748ad3b502a9526352415ead9c115e6
IamBansal <abansal11ae@gmail.com>
2022-08-07 16:16:33 +0530

    Arrays Questions LeetCode

---
+++ src/leetcode_arays/Question20.java
@@ -0,0 +1,43 @@
+package leetcode_arays;
+
+public class Question20 {
+
+    /*
+You are given row x col grid representing a map where grid[i][j] = 1 represents land and grid[i][j] = 0 represents water.
+
+Grid cells are connected horizontally/vertically (not diagonally).
+The grid is completely surrounded by water, and there is exactly one island
+(i.e., one or more connected land cells).
+
+The island doesn't have "lakes", meaning the water inside isn't connected to the water around the island.
+One cell is a square with side length 1. The grid is rectangular,
+ width and height don't exceed 100. Determine the perimeter of the island.
+     */
+
+    public static int islandPerimeter(int[][] grid) {
+        int perimeter = 0;
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == 1) {
+                    perimeter += 4;
+                    if (j != grid[0].length - 1 && grid[i][j + 1] == 1) {
+                        perimeter -= 2;
+                    }
+                    if (i != grid.length - 1 && grid[i + 1][j] == 1) {
+                        perimeter -= 2;
+                    }
+                }
+            }
+        }
+        return perimeter;
+    }
+
+    public static void main(String[] args) {
+        int[][] grid = {{0, 1, 0, 0}, {1, 1, 1, 0}, {0, 1, 0, 0}, {1, 1, 0, 0}};
+        int[][] grid1 = {{1}};
+        int[][] grid2 = {{1, 0}};
+        System.out.println(islandPerimeter(grid));
+        System.out.println(islandPerimeter(grid1));
+        System.out.println(islandPerimeter(grid2));
+    }
+}
