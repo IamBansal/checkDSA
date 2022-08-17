98d24550259b600970b203a2ff116cd68f5980e7
IamBansal <abansal11ae@gmail.com>
2022-08-17 13:15:02 +0530

    Strings Questions LeetCode

--- src/leetcode_strings/Question18.java
+++ src/leetcode_strings/Question18.java
@@ -8,8 +8,8 @@ public class Question18 {
     Given two non-negative integers, num1 and num2 represented as string,
     return the sum of num1 and num2 as a string.
 
-You must solve the problem without using any built-in library for handling large integers (such as BigInteger).
-You must also not convert the inputs to integers directly.
+     You must solve the problem without using any built-in library for handling large integers (such as BigInteger).
+     You must also not convert the inputs to integers directly.
      */
 
     public static String addStrings(String num1, String num2) {
@@ -18,10 +18,27 @@ You must also not convert the inputs to integers directly.
         return String.valueOf(number1.add(number2));
     }
 
+    public static String addStringsLogical(String num1, String num2) {
+        int i = num1.length() - 1;
+        int j = num2.length() - 1;
+        int carry = 0;
+        StringBuilder ans = new StringBuilder();
+        while (i >= 0 || j >= 0 || carry != 0) {
+            int val1 = i >= 0 ? num1.charAt(i) - '0' : 0;
+            int val2 = j >= 0 ? num2.charAt(j) - '0' : 0;
+            i--;
+            j--;
+            int sum = val1 + val2 + carry;
+            carry = sum / 10;
+            ans.append(sum % 10);
+        }
+        return ans.reverse().toString();
+    }
+
     public static void main(String[] args) {
-        System.out.println(addStrings("11", "123"));
-        System.out.println(addStrings("456", "77"));
-        System.out.println(addStrings("0", "0"));
-        System.out.println(addStrings("3876620623801494171", "6529364523802684779"));
+        System.out.println(addStringsLogical("11", "123"));
+        System.out.println(addStringsLogical("456", "77"));
+        System.out.println(addStringsLogical("0", "0"));
+        System.out.println(addStringsLogical("3876620623801494171", "6529364523802684779"));
     }
 }
---
+++ src/leetcode_strings/Question19.java
@@ -0,0 +1,33 @@
+package leetcode_strings;
+
+import java.util.ArrayList;
+
+public class Question19 {
+
+    /*
+    Given a string s, return the number of segments in the string.
+
+    A segment is defined to be a contiguous sequence of non-space characters.
+     */
+
+    public static int countSegments(String s) {
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) != ' ' && (i == 0 || s.charAt(i - 1) == ' ')) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(countSegments("Hello, my name is Akshat"));
+        System.out.println(countSegments("Hello"));
+        System.out.println(countSegments("H  e"));
+        System.out.println(countSegments(" H  e"));
+        System.out.println(countSegments(""));
+        System.out.println(countSegments("                     "));
+        System.out.println(countSegments("Of all the gin joints in all the towns in all the world,   "));
+        System.out.println(countSegments(", , , ,        a, eaefa"));
+    }
+}
