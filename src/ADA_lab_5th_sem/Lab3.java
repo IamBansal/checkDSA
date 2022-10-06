eb548a90dafd07f4141205ea0a44834d62b0995f
IamBansal <abansal11ae@gmail.com>
2022-10-06 14:11:53 +0530

    ADA LabWork

---
+++ src/ADA_lab_5th_sem/Lab3.java
@@ -0,0 +1,32 @@
+package ADA_lab_5th_sem;
+
+public class Lab3 {
+
+    public static void multiplyMatrix(int[][] arr1, int[][]arr2) {
+
+        int[][] result = new int[arr1.length][arr2[0].length];
+        if (arr2.length != arr1[0].length) {
+            System.out.println("Condition not satisfied.");
+            return;
+        }
+
+        for(int i = 0; i < arr1.length; i++){
+            for (int j = 0; j < arr2[0].length; j++){
+                result[i][j] = 0;
+                for (int k = 0; k < arr2[0].length; k++){
+                    result[i][j] += arr1[i][k] * arr2[k][j];
+
+                }
+                System.out.print(result[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int[][] arr1 = {{1, 1, 1}, {2, 2, 2}, {3, 3, 3}};
+        int[][] arr2 = {{1, 1, 1}, {2, 2, 2}, {3, 3, 3}};
+        multiplyMatrix(arr1, arr2);
+    }
+
+}
