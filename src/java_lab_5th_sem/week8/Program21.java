bc9a0145434ed9edff78b7d604de270807b9b9c0
IamBansal <abansal11ae@gmail.com>
2022-11-08 18:53:47 +0530

    JAVA LabWork

---
+++ src/java_lab_5th_sem/week8/Program21.java
@@ -0,0 +1,199 @@
+package java_lab_5th_sem.week8;
+
+import javax.swing.*;
+import java.io.*;
+import java.awt.event.*;
+import javax.swing.plaf.metal.*;
+
+//Text Editor using swing
+class editor extends JFrame implements ActionListener {
+    JTextArea t;
+
+    JFrame f;
+
+    editor(){
+        // Create a frame
+        f = new JFrame("editor");
+
+        try {
+            // Set metal look and feel
+            UIManager.setLookAndFeel("javax.swing.plaf.metal.MetalLookAndFeel");
+
+            // Set theme to ocean
+            MetalLookAndFeel.setCurrentTheme(new OceanTheme());
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        // Text component
+        t = new JTextArea();
+
+        // Create a main menu bar
+        JMenuBar mb = new JMenuBar();
+
+        // Create a menu item for main menu
+        JMenu m1 = new JMenu("File");
+
+        // Create menu items
+        JMenuItem mi1 = new JMenuItem("New");
+        JMenuItem mi2 = new JMenuItem("Open");
+        JMenuItem mi3 = new JMenuItem("Save");
+        JMenuItem mi9 = new JMenuItem("Print");
+
+        // Add action listener to menu items
+        mi1.addActionListener(this);
+        mi2.addActionListener(this);
+        mi3.addActionListener(this);
+        mi9.addActionListener(this);
+
+        m1.add(mi1);
+        m1.add(mi2);
+        m1.add(mi3);
+        m1.add(mi9);
+
+        // Create a menu item for main menu
+        JMenu m2 = new JMenu("Edit");
+
+        // Create menu items
+        JMenuItem mi4 = new JMenuItem("Cut");
+        JMenuItem mi5 = new JMenuItem("Copy");
+        JMenuItem mi6 = new JMenuItem("Paste");
+
+        // Add action listener to menu items
+        mi4.addActionListener(this);
+        mi5.addActionListener(this);
+        mi6.addActionListener(this);
+
+        m2.add(mi4);
+        m2.add(mi5);
+        m2.add(mi6);
+
+        // Create a menu item for main menu
+        JMenuItem mc = new JMenuItem("Close");
+
+        mc.addActionListener(this);
+
+        mb.add(m1);
+        mb.add(m2);
+        mb.add(mc);
+
+        f.setJMenuBar(mb);
+        f.add(t);
+        f.setSize(500, 500);
+        f.show();
+    }
+
+    // If a button is pressed
+    public void actionPerformed(ActionEvent e){
+        String s = e.getActionCommand();
+
+        switch (s) {
+            case "Cut":
+                t.cut();
+                break;
+            case "Copy":
+                t.copy();
+                break;
+            case "Paste":
+                t.paste();
+                break;
+            case "Save": {
+                // Create an object of JFileChooser class
+                JFileChooser j = new JFileChooser("f:");
+
+                // Invoke the showsSaveDialog function to show the save dialog
+                int r = j.showSaveDialog(null);
+
+                if (r == JFileChooser.APPROVE_OPTION) {
+
+                    // Set the label to the path of the selected directory
+                    File fi = new File(j.getSelectedFile().getAbsolutePath());
+
+                    try {
+                        // Create a file writer
+                        FileWriter wr = new FileWriter(fi, false);
+
+                        // Create buffered writer to write
+                        BufferedWriter w = new BufferedWriter(wr);
+
+                        // Write
+                        w.write(t.getText());
+
+                        w.flush();
+                        w.close();
+                    } catch (Exception evt) {
+                        JOptionPane.showMessageDialog(f, evt.getMessage());
+                    }
+                }
+                // If the user cancelled the operation
+                else
+                    JOptionPane.showMessageDialog(f, "The user cancelled the operation");
+                break;
+            }
+            case "Print":
+                try {
+                    // print the file
+                    t.print();
+                } catch (Exception evt) {
+                    JOptionPane.showMessageDialog(f, evt.getMessage());
+                }
+                break;
+            case "Open": {
+                // Create an object of JFileChooser class
+                JFileChooser j = new JFileChooser("f:");
+
+                // Invoke the showsOpenDialog function to show the save dialog
+                int r = j.showOpenDialog(null);
+
+                // If the user selects a file
+                if (r == JFileChooser.APPROVE_OPTION) {
+                    // Set the label to the path of the selected directory
+                    File fi = new File(j.getSelectedFile().getAbsolutePath());
+
+                    try {
+                        // String
+                        String s1 = "";
+                        StringBuilder sl = new StringBuilder();
+
+                        // File reader
+                        FileReader fr = new FileReader(fi);
+
+                        // Buffered reader
+                        BufferedReader br = new BufferedReader(fr);
+
+                        // Initialize sl
+                        sl = new StringBuilder(br.readLine());
+
+                        // Take the input from the file
+                        while ((s1 = br.readLine()) != null) {
+                            sl.append("\n").append(s1);
+                        }
+
+                        // Set the text
+                        t.setText(sl.toString());
+                    } catch (Exception evt) {
+                        JOptionPane.showMessageDialog(f, evt.getMessage());
+                    }
+                }
+                // If the user cancelled the operation
+                else
+                    JOptionPane.showMessageDialog(f, "The user cancelled the operation");
+                break;
+            }
+            case "New":
+                t.setText("");
+                break;
+            case "Close":
+                f.setVisible(false);
+                break;
+        }
+    }
+
+}
+
+public class Program21 {
+    public static void main(String[] args)
+    {
+        new editor();
+    }
+}
