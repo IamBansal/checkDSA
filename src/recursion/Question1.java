b5e094e7802e5e9c96b7613f450db9d16250bc3c
IamBansal <abansal11ae@gmail.com>
2023-02-27 23:36:25 +0530

    Recursion Questions

---
+++ src/recursion/Question1.java
@@ -0,0 +1,119 @@
+package recursion;
+
+import javax.sound.midi.Soundbank;
+import java.util.Vector;
+
+public class Question1 {
+
+    //Print 1 to n
+    public static void print1ton(int n){
+
+        if (n == 1){
+            System.out.print(n + " ");
+            return;
+        }
+
+        print1ton(n -1);
+        System.out.print(n + " ");
+
+    }
+
+    //Print n to 1
+    public static void printNto1(int n){
+
+        if (n == 1){
+            System.out.print(n + " ");
+            return;
+        }
+
+        System.out.print(n + " ");
+        printNto1(n -1);
+    }
+
+    //factorial value
+    public static int factorial(int n){
+        if (n == 0 || n == 1){
+            return n;
+        }
+
+        return n * factorial(n-1);
+
+    }
+
+    //Sort an array
+    public static void sortArray(Vector<Integer> arr){
+
+        if (arr.size() == 0 || arr.size() == 1){
+            return;
+        }
+
+        int temp = arr.elementAt(arr.size() - 1);
+
+        arr.removeElementAt(arr.size() - 1);
+        sortArray(arr);
+        insert(arr, temp);
+        printVector(arr);
+
+    }
+
+    private static void insert(Vector<Integer> arr, int temp) {
+
+        if (arr.size() == 0 || arr.elementAt(arr.size() - 1) <= temp){
+            arr.addElement(temp);
+            return;
+        }
+
+        int val = arr.elementAt(arr.size() - 1);
+
+        arr.removeElementAt(arr.size() - 1);
+        insert(arr, temp);
+        arr.addElement(val);
+
+    }
+
+    public static void printVector(Vector<Integer> arr){
+        for (Integer e: arr
+             ) {
+            System.out.print(e + " ");
+        }
+        System.out.println();
+    }
+
+    public static int fib(int n) {
+        if(n == 0 || n == 1){
+            return n;
+        }
+
+        return fib(n-1) + fib(n-2);
+
+    }
+
+
+
+
+    public static void main(String[] args) {
+
+//        print1ton(7);
+//        System.out.println();
+//
+//        printNto1(7);
+//        System.out.println();
+//
+//        System.out.println(factorial(5));
+//
+//        Vector<Integer> input = new Vector<>();
+//        input.add(3);
+//        input.add(5);
+//        input.add(6);
+//        input.add(2);
+//        input.add(1);
+//        input.add(0);
+//        sortArray(input);
+
+        System.out.println(fib(3));
+        System.out.println(fib(7));
+        System.out.println(fib(5));
+
+    }
+
+}
---
+++ src/recursion/Question5.java
@@ -0,0 +1,112 @@
+package recursion;
+
+public class Question5 {
+
+    /*
+    DESCRIPTION :
+
+    You are given two non-empty linked lists representing two non-negative integers.
+    The digits are stored in reverse order, and each of their nodes contains a single digit.
+    Add the two numbers and return the sum as a linked list.
+    You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+
+     */
+
+    public static Question2.ListNode addTwoNumbers(Question2.ListNode l1, Question2.ListNode l2) {
+
+        if (l1 == null) {
+            return l2;
+        }
+        if (l2 == null) {
+            return l1;
+        }
+
+        Question2.ListNode node = new Question2.ListNode();
+
+        int sum = l1.val + l2.val;
+        if (sum > 9) {
+            node.val = sum % 10;
+            node.next = addTwoNumber(l1.next, l2.next, sum / 10);
+        } else {
+            node.val = sum;
+            node.next = addTwoNumber(l1.next, l2.next, 0);
+        }
+        return node;
+    }
+
+    private static Question2.ListNode addTwoNumber(Question2.ListNode l1, Question2.ListNode l2, int carry) {
+        Question2.ListNode node = new Question2.ListNode();
+
+        if (l1 == null && l2 == null) {
+            if (carry == 0) {
+                return null;
+            } else {
+                if (carry > 9) {
+                    node.val = carry % 10;
+                    node.next = addTwoNumber(null, null, carry / 10);
+                } else {
+                    node.val = carry;
+                    node.next = addTwoNumber(null, null, 0);
+                }
+
+            }
+        }
+
+        else if (l1 == null) {
+            int sum = l2.val + carry;
+            if (sum > 9) {
+                node.val = sum % 10;
+                node.next = addTwoNumber(null, l2.next, sum / 10);
+            } else {
+                node.val = sum;
+                node.next = addTwoNumber(null, l2.next, 0);
+            }
+        } else if (l2 == null) {
+            int sum = l1.val + carry;
+            if (sum > 9) {
+                node.val = sum % 10;
+                node.next = addTwoNumber(l1.next, null, sum / 10);
+            } else {
+                node.val = sum;
+                node.next = addTwoNumber(l1.next, null, 0);
+            }
+        } else {
+            int sum = l1.val + l2.val + carry;
+            if (sum > 9) {
+                node.val = sum % 10;
+                node.next = addTwoNumber(l1.next, l2.next, sum / 10);
+            } else {
+                node.val = sum;
+                node.next = addTwoNumber(l1.next, l2.next, 0);
+            }
+
+        }
+        return node;
+    }
+
+    public static void main(String[] args) {
+
+        Question2.ListNode l1 = new Question2.ListNode(9);
+        Question2.ListNode l2 = new Question2.ListNode(9);
+        Question2.ListNode l3 = new Question2.ListNode(9);
+
+        Question2.ListNode l4 = new Question2.ListNode(9);
+        Question2.ListNode l5 = new Question2.ListNode(9);
+        Question2.ListNode l6 = new Question2.ListNode(9);
+        Question2.ListNode l7 = new Question2.ListNode(9);
+
+        l1.next = l2;
+        l2.next = l3;
+
+        l4.next = l5;
+        l5.next = l6;
+        l6.next = l7;
+
+        Question2.ListNode result = addTwoNumbers(l1, l4);
+
+        while (result != null) {
+            System.out.print(result.val + " ");
+            result = result.next;
+        }
+    }
+}
