33783086200e5da34c3c1c65bd840005d36ab02f
IamBansal <abansal11ae@gmail.com>
2023-03-07 15:59:45 +0530

    DP Questions

---
+++ src/leetcode_dynamic_programming/Question11.java
@@ -0,0 +1,43 @@
+package leetcode_dynamic_programming;
+
+public class Question11 {
+
+    /*
+    62. Unique Paths
+    There is a robot on an m x n grid.
+    The robot is initially located at the top-left corner (i.e., grid[0][0]).
+    The robot tries to move to the bottom-right corner (i.e., grid[m - 1][n - 1]).
+    The robot can only move either down or right at any point in time.
+    Given the two integers m and n, return the number of possible unique paths
+    that the robot can take to reach the bottom-right corner.
+
+    The test cases are generated so that the answer will be less than or equal to 2 * 109.
+
+     */
+
+
+    public static int uniquePaths(int m, int n) {
+
+        int[][] dp = new int[m][n];
+        for (int i = 0; i < dp.length; i++) {
+            for (int j = 0; j < dp[0].length; j++) {
+                if (i == 0 && j == 0) dp[i][j] = 1;
+                else {
+                    int up = 0, left = 0;
+                    if (i > 0) up = dp[i - 1][j];
+                    if (j > 0) left = dp[i][j - 1];
+                    dp[i][j] = left + up;
+                }
+            }
+        }
+        return dp[dp.length - 1][dp[0].length - 1];
+
+
+    }
+
+    public static void main(String[] args) {
+        System.out.println(uniquePaths(3, 7));
+        System.out.println(uniquePaths(3, 2));
+    }
+
+}
