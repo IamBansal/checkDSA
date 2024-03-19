a11c063925446e44d54b339cc7a0afeb25d59fdd
AKSHAT BANSAL <76641123+IamBansal@users.noreply.github.com>
2024-03-19 23:38:44 +0530

    Update SearchInRotatedSortedArray.java

--- src/leetcode_problems/sheet_arsh/searching_sorting/SearchInRotatedSortedArray.java
+++ src/leetcode_problems/sheet_arsh/searching_sorting/SearchInRotatedSortedArray.java
@@ -4,6 +4,7 @@ public class SearchInRotatedSortedArray {
 
     /*
     33. Search in Rotated Sorted Array
+    
     There is an integer array nums sorted in ascending order (with distinct values).
     Prior to being passed to your function, nums is possibly rotated at an unknown
     pivot index k (1 <= k < nums.length) such that the resulting array is
