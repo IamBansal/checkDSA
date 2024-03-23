a5965c51342ebbef1588f4e9da683525be950b10
AKSHAT BANSAL <76641123+IamBansal@users.noreply.github.com>
2024-03-23 22:23:44 +0530

    Update

--- src/leetcode_problems/sheet_arsh/searching_sorting/SearchInRotatedSortedArray.java
+++ src/leetcode_problems/sheet_arsh/searching_sorting/SearchInRotatedSortedArray.java
@@ -34,5 +34,6 @@ public class SearchInRotatedSortedArray {
     public static void main(String[] args) {
         System.out.println(search(new int[]{4, 5, 6, 7, 0, 1, 2}, 0));
         System.out.println(search(new int[]{4, 5, 6, 7, 0, 1, 2}, 3));
+        System.out.println(search(new int[]{4, 5, 6, 7, 0, 1, 2}, 7));
     }
 }
